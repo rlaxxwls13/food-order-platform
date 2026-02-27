2af86ef4f240ebff48f7bdccdb4adf13c1e7485e
sionkim0126 <kimsion000@naver.com>
2026-02-27 14:12:48 +0900

    add : AddressEntity

---
+++ src/main/java/nbcamp/food_order_platform/user/entity/AddressEntity.java
@@ -0,0 +1,30 @@
+package nbcamp.food_order_platform.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity(name = "p_address")
+@Getter
+@NoArgsConstructor
+public class AddressEntity {
+
+    @Id
+    @GeneratedValue
+    private UUID addressId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @Column(name = "place_name")
+    private String placeName;
+
+    @Column(name = "road_name")
+    private String roadName;
+
+    @Column(name = "detail_name")
+    private String detailName;
+}
