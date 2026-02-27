9c270c26414a3fc479683d3706f356cc07d0e19c
sionkim0126 <kimsion000@naver.com>
2026-02-27 15:04:37 +0900

    update : BaseEntity isDeleted 확인 메서드 추가

--- src/main/java/nbcamp/food_order_platform/global/common/entity/BaseEntity.java
+++ src/main/java/nbcamp/food_order_platform/global/common/entity/BaseEntity.java
@@ -4,40 +4,47 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
-public class BaseEntity {
+public abstract class BaseEntity {
 
     @CreatedDate
-    @Column(name = "created_at",updatable = false)
+    @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
-    @Column(name = "created_by")
-    private UUID createdBy;
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private Long createdBy;
 
     @LastModifiedDate
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
-    @Column(name = "update_by")
-    private UUID updatedBy;
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private Long updatedBy;
 
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
 
     @Column(name = "deleted_by")
-    private UUID deletedBy;
+    private Long deletedBy;
 
-    public void softDelete(UUID userId){
+    public void softDelete(Long userId) {
         this.deletedAt = LocalDateTime.now();
         this.deletedBy = userId;
+    }
+
+    public boolean isDeleted() {
+        return this.deletedAt != null;
     }
 }
