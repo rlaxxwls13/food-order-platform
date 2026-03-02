528e78792040f120f358cf4d23e3b34046e513d2
Lee Subin <99sbin@gmail.com>
2026-03-02 20:20:38 +0900

    fix : CreateReviewDto User 객체로 변경

--- src/main/java/nbcamp/food_order_platform/review/application/dto/CreateReviewDto.java
+++ src/main/java/nbcamp/food_order_platform/review/application/dto/CreateReviewDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import nbcamp.food_order_platform.user.domain.User;
 
 import java.util.UUID;
 
@@ -15,8 +16,7 @@ public class CreateReviewDto { // 리뷰 작성, service에서 사용
 
     private UUID orderId;
     private UUID storeId;
-    private Long userId;
-    private String nickname;
+    private User user; // Long userId → User 객체로 변경
     private int rating;
     private String content;
 
