934775e5d4d68abf295f87dd26584e8310ae65da
Lee Subin <99sbin@gmail.com>
2026-03-04 12:56:00 +0900

    move : req,res dto 패키지 분리

--- src/main/java/nbcamp/food_order_platform/review/presentation/dto/response/PatchReviewResDto.java
+++ src/main/java/nbcamp/food_order_platform/review/presentation/dto/response/PatchReviewResDto.java
@@ -1,9 +1,8 @@
-package nbcamp.food_order_platform.review.presentation.dto;
+package nbcamp.food_order_platform.review.presentation.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import nbcamp.food_order_platform.review.domain.entity.Review;
 import nbcamp.food_order_platform.review.domain.entity.ReviewStatus;
 
@@ -11,30 +10,28 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
-public class PostReviewResDto { // 리뷰 작성 응답
+@AllArgsConstructor
+public class PatchReviewResDto {
 
     private UUID reviewId;
     private UUID storeId;
     private String nickname;
-
     private int rating;
     private String content;
     private ReviewStatus status;
-    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
-    public static PostReviewResDto from(Review review) {
-        return PostReviewResDto.builder()
+    public static PatchReviewResDto from(Review review) {
+        return PatchReviewResDto.builder()
                 .reviewId(review.getReviewId())
                 .storeId(review.getStoreId())
                 .nickname(review.getNickname())
                 .rating(review.getRating())
                 .content(review.getContent())
                 .status(review.getStatus())
-                .createdAt(review.getCreatedAt())
+                .updatedAt(review.getUpdatedAt())
                 .build();
     }
-
 }
+
--- src/main/java/nbcamp/food_order_platform/review/presentation/dto/response/PostReviewResDto.java
+++ src/main/java/nbcamp/food_order_platform/review/presentation/dto/response/PostReviewResDto.java
@@ -1,8 +1,9 @@
-package nbcamp.food_order_platform.review.presentation.dto;
+package nbcamp.food_order_platform.review.presentation.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import nbcamp.food_order_platform.review.domain.entity.Review;
 import nbcamp.food_order_platform.review.domain.entity.ReviewStatus;
 
@@ -10,28 +11,30 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
-@Builder
+@NoArgsConstructor
 @AllArgsConstructor
-public class PatchReviewResDto {
+@Builder
+public class PostReviewResDto { // 리뷰 작성 응답
 
     private UUID reviewId;
     private UUID storeId;
     private String nickname;
+
     private int rating;
     private String content;
     private ReviewStatus status;
-    private LocalDateTime updatedAt;
+    private LocalDateTime createdAt;
 
-    public static PatchReviewResDto from(Review review) {
-        return PatchReviewResDto.builder()
+    public static PostReviewResDto from(Review review) {
+        return PostReviewResDto.builder()
                 .reviewId(review.getReviewId())
                 .storeId(review.getStoreId())
                 .nickname(review.getNickname())
                 .rating(review.getRating())
                 .content(review.getContent())
                 .status(review.getStatus())
-                .updatedAt(review.getUpdatedAt())
+                .createdAt(review.getCreatedAt())
                 .build();
     }
-}
 
+}
