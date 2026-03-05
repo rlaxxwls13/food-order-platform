8982deee91145ccd06dcd49401aeb1a1d01846d2
juneon <juneon0830@naver.com>
2026-03-05 12:46:14 +0900

    feat: store 엔티티 반영

--- src/main/java/nbcamp/food_order_platform/store/domain/entity/Store.java
+++ src/main/java/nbcamp/food_order_platform/store/domain/entity/Store.java
@@ -2,12 +2,15 @@ package nbcamp.food_order_platform.store.domain.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import nbcamp.food_order_platform.category.domain.entity.Category;
 import nbcamp.food_order_platform.global.common.BaseEntity;
+import nbcamp.food_order_platform.regionCode.domain.entity.RegionCode;
 import org.hibernate.annotations.SQLRestriction;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -25,8 +28,16 @@ public class Store extends BaseEntity {
     @Column(name = "owner_id", nullable = false)
     private Long ownerId;
 
-    @Column(name = "region_id", nullable = false)
-    private Long regionId;
+    @OneToOne(
+            mappedBy = "store",
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL,
+            optional = false
+    )
+    private StoreRegion storeRegion;
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+    private List<StoreCategory> storeCategories = new ArrayList<>();
 
     @Column(name = "name", nullable = false, length = 255)
     private String name;
@@ -41,21 +52,42 @@ public class Store extends BaseEntity {
     @Column(name = "version", nullable = false)
     private Long version;
 
-    @Builder
-    public Store(Long ownerId, Long regionId, String name) {
-        validate(ownerId, regionId, name);
+    public Store(Long ownerId, String name, RegionCode regionCode, String addressDetail, List<Category> categories) {
+        validate(ownerId, regionCode, addressDetail, name);
         this.ownerId = ownerId;
-        this.regionId = regionId;
+        this.storeRegion = StoreRegion.create(this, regionCode, addressDetail);
+        this.storeCategories.addAll(makeStoreCategoryFromCategory(this, categories));
         this.name = name;
         this.totalRatingSum = 0;
         this.reviewCount = 0;
     }
 
-    private static void validate(Long ownerId, Long regionId, String name) {
-        if(ownerId == null || regionId == null || name == null || name.isBlank())
-            throw new IllegalArgumentException("ownerId, regionId, name은 필수입니다.");
+    public void softDelete(Long userId) {
+        if (storeRegion != null) {
+            storeRegion.softDelete(userId);
+        }
+        for (StoreCategory sc : storeCategories) {
+            sc.softDelete(userId);
+        }
+        super.softDelete(userId);
+    }
+
+    private static void validate(Long ownerId, RegionCode regionCode, String addressDetail, String name) {
+        if(ownerId == null || regionCode == null || name == null || name.isBlank() || addressDetail == null || addressDetail.isBlank())
+            throw new IllegalArgumentException("ownerId, regionCode, addressDetail, name은 필수입니다.");
         if(name.length() > 255)
             throw new IllegalArgumentException("name(가게명)은 255자 이하여야합니다.");
+    }
+
+    private List<StoreCategory> makeStoreCategoryFromCategory(Store store, List<Category> categories){
+        if(categories == null || categories.isEmpty())
+            return List.of();
+
+        List<StoreCategory> storeCategoryList = new ArrayList<>();
+        for(Category category : categories) {
+            storeCategoryList.add(StoreCategory.create(store, category));
+        }
+        return storeCategoryList;
     }
 
     public void changeOwner(Long ownerId) {
@@ -64,10 +96,31 @@ public class Store extends BaseEntity {
         this.ownerId = ownerId;
     }
 
-    public void changeRegion(Long regionId) {
-        if(regionId == null)
-            throw new IllegalArgumentException("regionId(지역 아이디)가 비어있습니다.");
-        this.regionId = regionId;
+    public void changeRegion(RegionCode regionCode, String addressDetail) {
+        if(regionCode == null || addressDetail == null || addressDetail.isBlank())
+            throw new IllegalArgumentException("regionCode(지역) 또는 addressDetail(상세 주소)이 비어있습니다.");
+        this.storeRegion.change(regionCode, addressDetail);
+    }
+
+    public void addCategory(Category category) {
+        if (category == null)
+            throw new IllegalArgumentException("category가 비어있습니다.");
+        boolean exists = this.storeCategories.stream()
+                .anyMatch(sc -> sc.getCategory().getId().equals(category.getId()));
+        if (exists) return;
+
+        this.storeCategories.add(StoreCategory.create(this, category));
+    }
+
+    public void removeCategory(Category category, Long userId) {
+        if (category == null)
+            throw new IllegalArgumentException("category가 비어있습니다.");
+        if (userId == null)
+            throw new IllegalArgumentException("userId가 비어있습니다.");
+
+        this.storeCategories.stream()
+                .filter(sc -> sc.getCategory().getId().equals(category.getId()))
+                .forEach(sc -> sc.softDelete(userId));
     }
 
     public void changeName(String name) {
