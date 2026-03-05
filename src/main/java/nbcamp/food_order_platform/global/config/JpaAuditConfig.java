e97dd79dae26a8be0725cbf939ad690268cd7ed6
Lee Subin <99sbin@gmail.com>
2026-03-06 00:20:15 +0900

    refactor: JPA Auditing 설정을 별도 Config 클래스로 분리

---
+++ src/main/java/nbcamp/food_order_platform/global/config/JpaAuditConfig.java
@@ -0,0 +1,9 @@
+package nbcamp.food_order_platform.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditConfig {
+}
