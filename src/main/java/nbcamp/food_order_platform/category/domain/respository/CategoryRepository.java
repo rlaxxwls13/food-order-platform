0366f64c53dd926e15a7f5a4c29f0eab843516d4
juneon <juneon0830@naver.com>
2026-03-05 17:28:32 +0900

    bugfix : repository 식별자 변경 및 store 매서드 이름 변경

--- src/main/java/nbcamp/food_order_platform/category/domain/respository/CategoryRepository.java
+++ src/main/java/nbcamp/food_order_platform/category/domain/respository/CategoryRepository.java
@@ -3,5 +3,7 @@ package nbcamp.food_order_platform.category.domain.respository;
 import nbcamp.food_order_platform.category.domain.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+import java.util.UUID;
+
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
 }
--- src/main/java/nbcamp/food_order_platform/regionCode/domain/repository/RegionCodeRepository.java
+++ src/main/java/nbcamp/food_order_platform/regionCode/domain/repository/RegionCodeRepository.java
@@ -3,5 +3,7 @@ package nbcamp.food_order_platform.regionCode.domain.repository;
 import nbcamp.food_order_platform.regionCode.domain.entity.RegionCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RegionCodeRepository extends JpaRepository<RegionCode, Long> {
+import java.util.UUID;
+
+public interface RegionCodeRepository extends JpaRepository<RegionCode, UUID> {
 }
