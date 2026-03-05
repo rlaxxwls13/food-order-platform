45dfbba629fd4de292a15aaef5ae73305c73879d
zlon <zlon@outlook.kr>
2026-03-05 17:12:29 +0900

    refactor: 단방향 의존을 위해 application DTO 기반 구조로 변경

--- src/main/java/nbcamp/food_order_platform/product/presentation/dto/response/PostProductResDto.java
+++ src/main/java/nbcamp/food_order_platform/product/presentation/dto/response/PostProductResDto.java
@@ -1,7 +1,6 @@
 package nbcamp.food_order_platform.product.presentation.dto.response;
 
 import lombok.Getter;
-import nbcamp.food_order_platform.product.application.dto.result.CreateProductResult;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -16,16 +15,4 @@ public class PostProductResDto {
     private String description;
     private boolean isHidden;
     private LocalDateTime createdAt;
-
-
-    public PostProductResDto(CreateProductResult result) {
-        this.productId = result.getProductId();
-        this.storeId = result.getStoreId();
-        this.name = result.getName();
-        this.price = result.getPrice();
-        this.stockQuantity = result.getStockQuantity();
-        this.description = result.getDescription();
-        this.isHidden = result.isHidden();
-        this.createdAt = result.getCreatedAt();
-    }
 }
