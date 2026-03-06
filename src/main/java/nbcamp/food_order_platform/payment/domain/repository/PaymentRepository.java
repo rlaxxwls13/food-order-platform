ca42d56733e744651a7ff369cb57064edcfd7253
ansdl <ansdlsgur0123@gmail.com>
2026-03-06 13:18:16 +0900

    feat: order service logic update

--- src/main/java/nbcamp/food_order_platform/payment/domain/repository/PaymentRepository.java
+++ src/main/java/nbcamp/food_order_platform/payment/domain/repository/PaymentRepository.java
@@ -3,7 +3,11 @@ package nbcamp.food_order_platform.payment.domain.repository;
 import nbcamp.food_order_platform.payment.domain.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+
+    // 특정 유저의 결제 목록 조회 (Payment → Order → User 경로)
+    List<Payment> findAllByOrderUserUserId(Long userId);
 }
