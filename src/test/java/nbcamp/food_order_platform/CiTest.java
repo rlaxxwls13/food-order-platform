2862c65d306bd40790c18db7f9035fda305b580c
rlaxxwls13 <rlaxxwls13@hufs.ac.kr>
2026-03-06 16:23:22 +0900

    test: ci test용 테스트 작성 Related to: #47

---
+++ src/test/java/nbcamp/food_order_platform/CiTest.java
@@ -0,0 +1,14 @@
+package nbcamp.food_order_platform;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CiTest {
+
+    @Test
+    void ciTest() {
+        int result = 1 + 1;
+        assertEquals(2, result);
+    }
+}
