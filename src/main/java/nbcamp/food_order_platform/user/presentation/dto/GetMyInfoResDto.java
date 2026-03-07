c1e02a8fc4979625666dc06209e9c8cbe448750f
sionkim0126 <kimsion000@naver.com>
2026-03-07 23:42:58 +0900

    [feat] 권한처리 기능 구현 (#53)
    * feat: JWT 인증 필터 및 권한 처리 설정
    
    * add : 사용자 조회 Dto, Result
    
    * add : 사용자 상세 조회 Dto, Result
    
    * add : 권한변경 Dto, Command
    
    * feat : 권한관련 api 구현

--- src/main/java/nbcamp/food_order_platform/user/presentation/dto/GetMyInfoResDto.java
+++ src/main/java/nbcamp/food_order_platform/user/presentation/dto/GetMyInfoResDto.java
@@ -1,6 +1,6 @@
 package nbcamp.food_order_platform.user.presentation.dto;
 
-public record GetUserResDto(
+public record GetMyInfoResDto(
         Long userId,
         String username,
         String role
