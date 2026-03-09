74aab6ce9add43b3591bf244ae48c13d7765bb5f
zlon <zlon@outlook.kr>
2026-03-09 19:51:01 +0900

    refactor: RestTemplate import제외

--- src/main/java/nbcamp/food_order_platform/ai/application/service/AiDescriptionService.java
+++ src/main/java/nbcamp/food_order_platform/ai/application/service/AiDescriptionService.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientResponseException;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
