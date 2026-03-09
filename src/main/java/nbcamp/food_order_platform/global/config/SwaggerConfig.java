112e62b69de68b0f4d25c665b7f01f653871702f
juneon <juneon0830@naver.com>
2026-03-09 19:59:24 +0900

    feat : swagger JWT 추가

--- src/main/java/nbcamp/food_order_platform/global/config/SwaggerConfig.java
+++ src/main/java/nbcamp/food_order_platform/global/config/SwaggerConfig.java
@@ -1,8 +1,10 @@
 package nbcamp.food_order_platform.global.config;
 
-import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +21,12 @@ public class SwaggerConfig {
                         .title("Food Order Platform API")
                         .description("음식 주문 플랫폼 API 문서")
                         .version("v1.0.0"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .servers(List.of(
                         new Server().url("http://localhost:8080").description("local server")
                 ));
