c07374d5e62d9769ab261f5b61575fe6a2aff430
sionkim0126 <kimsion000@naver.com>
2026-03-09 21:30:50 +0900

    [Update] 회원탈퇴 API구현 추가 및 ErrorCode 추가 (#75) (#81)
    * feat : refreshToken 재발급 api 구현
    
    * feat : 회원탈퇴 api 구현
    
    * update : ErrorCode 추가 및 적용

--- src/main/java/nbcamp/food_order_platform/global/error/ErrorCode.java
+++ src/main/java/nbcamp/food_order_platform/global/error/ErrorCode.java
@@ -8,12 +8,15 @@ public enum ErrorCode {
 
     //400
     VALIDATION_FAILED(HttpStatus.BAD_REQUEST, 4001, "Validation failed."), //요청실패
+    INVALID_ROLE(HttpStatus.BAD_REQUEST, 4002, "Invalid role"), //잘못된 role요청
+    ALREADY_DELETED_USER(HttpStatus.BAD_REQUEST, 4003, "User already deleted"), //이미 탈퇴한 사용자
 
     // 401
     SIGN_IN_FAIL(HttpStatus.UNAUTHORIZED, 4012, "Login information mismatch."), //로그인실패
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, 4013,"Invalid or expired token."), //토큰 만료
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, 4013,"Invalid or expired token."), //토큰 서명오류/형식 오류
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, 4014, "Invalid password."), //잘못된 패스워드
     AUTHORIZATION(HttpStatus.UNAUTHORIZED, 4015,"Authorization Failed."), //인증실패
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, 4016, "expired token"), //토큰 말료
 
     // 403
     NO_PERMISSION(HttpStatus.FORBIDDEN, 4031,"Do not have permission."), //권한없음
@@ -33,6 +36,7 @@ public enum ErrorCode {
     DUPLICATED_USER_ID(HttpStatus.CONFLICT, 4091, "User ID already exists."), //유저ID 중복
     INSUFFICIENT_STOCK(HttpStatus.CONFLICT, 4092, "Insufficient stock."), //재고부족
     REVIEW_ALREADY_EXISTS(HttpStatus.CONFLICT, 4093, "Review already exists for this order."), //이미 리뷰를 작성한 주문
+    DUPLICATED_EMAIL(HttpStatus.CONFLICT, 4094, "User email already exists"), // email 중복
 
     // 500
     DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 5001, "Database error."), //DB처리 오류
--- src/main/java/nbcamp/food_order_platform/user/domain/repository/UserRepository.java
+++ src/main/java/nbcamp/food_order_platform/user/domain/repository/UserRepository.java
@@ -4,6 +4,7 @@ import nbcamp.food_order_platform.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +15,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     Optional<User> findByUsername(String username);
+
+    List<User> findAllByDeletedAtIsNull();
 }
