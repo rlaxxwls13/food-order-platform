2b91582d54167a1550f06fa02e838f040a7b36fb
rlaxxwls13 <rlaxxwls13@hufs.ac.kr>
2026-03-09 16:57:58 +0900

    Fix: test active profiles prod로 변경

--- src/test/java/nbcamp/food_order_platform/store/StoreIntegrationTest.java
+++ src/test/java/nbcamp/food_order_platform/store/StoreIntegrationTest.java
@@ -32,7 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles(profiles = {"prod", "local"})
+@ActiveProfiles("prod")
 @Transactional
 class StoreIntegrationTest {
 
