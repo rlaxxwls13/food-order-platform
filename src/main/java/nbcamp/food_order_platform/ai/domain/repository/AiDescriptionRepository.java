6e261e03fb5bc7ee5d522b66a1568952203e5f2f
지온 <140367464+zlonce@users.noreply.github.com>
2026-03-10 07:13:23 +0900

    [Feat] AI 로그 조회 구현 (#65, #69) (#83)
    * refactor: StoreRepository 추가
    
    * feat: AI 로그 조회 Dto, Repository 구현
    
    * feat: AI 로그 조회 Service, Controller 구현
    
    * feat: AI 로그 조회 권한 추가

--- src/main/java/nbcamp/food_order_platform/ai/domain/repository/AiDescriptionRepository.java
+++ src/main/java/nbcamp/food_order_platform/ai/domain/repository/AiDescriptionRepository.java
@@ -1,9 +1,12 @@
 package nbcamp.food_order_platform.ai.domain.repository;
 
 import nbcamp.food_order_platform.ai.domain.entity.AiDescription;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface AiDescriptionRepository extends JpaRepository<AiDescription, UUID> {
+    Page<AiDescription> findByProductId(UUID productId, Pageable pageable);
 }
