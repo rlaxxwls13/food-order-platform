3baaa0d4e6c66eabb00b74062dc011bb0abac7cf
ansdl <ansdlsgur0123@gmail.com>
2026-03-10 11:15:49 +0900

    [Fix] 임시파일 삭제후 test 파일 수정

--- src/test/java/nbcamp/food_order_platform/order/domain/repository/OrderRepositoryTest.java
+++ src/test/java/nbcamp/food_order_platform/order/domain/repository/OrderRepositoryTest.java
@@ -10,6 +10,7 @@ import nbcamp.food_order_platform.user.domain.entity.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -21,6 +22,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class OrderRepositoryTest {
 
     @Autowired
--- src/test/java/nbcamp/food_order_platform/payment/domain/repository/PaymentRepositoryTest.java
+++ src/test/java/nbcamp/food_order_platform/payment/domain/repository/PaymentRepositoryTest.java
@@ -12,6 +12,7 @@ import nbcamp.food_order_platform.user.domain.entity.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -23,6 +24,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class PaymentRepositoryTest {
 
     @Autowired
