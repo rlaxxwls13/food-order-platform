1914b98baca565be5ea93aa809263a144692affd
sionkim0126 <kimsion000@naver.com>
2026-03-10 16:59:08 +0900

    add : UserServiceTest, AuthServiceTest

---
+++ src/test/java/nbcamp/food_order_platform/user/application/service/UserServiceTest.java
@@ -0,0 +1,98 @@
+package nbcamp.food_order_platform.user.application.service;
+
+import nbcamp.food_order_platform.global.error.exception.BusinessException;
+import nbcamp.food_order_platform.user.domain.entity.User;
+import nbcamp.food_order_platform.user.domain.repository.UserRepository;
+import nbcamp.food_order_platform.user.presentation.dto.SignupRequestDto;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    void 회원가입_성공() {
+
+        SignupRequestDto dto =
+                new SignupRequestDto(
+                        "testuse1",
+                        "test@email.com",
+                        "Password1!",
+                        "nickname"
+                );
+
+        given(userRepository.existsByUsername("testuse1"))
+                .willReturn(false);
+
+        given(userRepository.existsByEmail("test@email.com"))
+                .willReturn(false);
+
+        given(passwordEncoder.encode(any()))
+                .willReturn("encodedPassword");
+
+        userService.signup(dto);
+
+        verify(userRepository).save(any(User.class));
+    }
+
+    @Test
+    void 회원가입_실패_아이디중복() {
+
+        SignupRequestDto dto =
+                new SignupRequestDto(
+                        "testuse1",
+                        "test@email.com",
+                        "Password1!",
+                        "nickname"
+                );
+
+        given(userRepository.existsByUsername("testuse1"))
+                .willReturn(true);
+
+        assertThrows(
+                BusinessException.class,
+                () -> userService.signup(dto)
+        );
+    }
+
+    @Test
+    void 회원가입_실패_이메일중복() {
+
+        SignupRequestDto dto =
+                new SignupRequestDto(
+                        "testuse1",
+                        "test@email.com",
+                        "Password1!",
+                        "nickname"
+                );
+
+        given(userRepository.existsByUsername("testuse1"))
+                .willReturn(false);
+
+        given(userRepository.existsByEmail("test@email.com"))
+                .willReturn(true);
+
+        assertThrows(
+                BusinessException.class,
+                () -> userService.signup(dto)
+        );
+    }
+}
