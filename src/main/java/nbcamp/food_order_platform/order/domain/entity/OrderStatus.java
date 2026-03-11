1e69eac1160c9b582408d35b519dfa5f3b0acf39
ansdl <ansdlsgur0123@gmail.com>
2026-03-11 15:04:21 +0900

    [Fix] 환불 로직 Payment 로 이전 + 테스트 코드 작성

--- src/main/java/nbcamp/food_order_platform/order/domain/entity/OrderStatus.java
+++ src/main/java/nbcamp/food_order_platform/order/domain/entity/OrderStatus.java
@@ -11,15 +11,13 @@ public enum OrderStatus {
         CREATED --> PAID : 결제 완료
         CREATED --> CANCELED : 사용자 / 시스템취소 (미결제)
 
-        PAID --> ACCEPTED : 가게 승인 (조리 시작 / 상품 준비)
-        PAID -->  REJECTED : 가게 거절(재고 없음 / 가게 사정 등)
+        PAID --> STORE_ACCEPTED : 가게 승인 (조리 시작 / 상품 준비)
+        PAID --> STORE_REJECTED : 가게 거절(재고 없음 / 가게 사정 등)
 
-        ACCEPTED --> REFUNDED : 관리자 전용 취소
-        REJECTED --> REFUNDED : 자동 환불 처리
+        %% 환불 완료(Refunded)는 주문 상태가 아니라 결제(Payment) 상태에서 관리
+        %% PaymentStatus.REFUNDED 참고
 
-        ACCEPTED --> COMPLETED : 배송/픽업 완료
-
-       COMPLETED --> REFUNDED 현재 설계 단계에선 불가x
+        STORE_ACCEPTED --> COMPLETED : 배송/픽업 완료 (현재 코드 미구현)
 
     * */
     CREATED("생성 완료"),
@@ -27,7 +25,6 @@ public enum OrderStatus {
     CANCELED("결제 취소"),// 결제 전 취소
     STORE_ACCEPTED("가게 승인"),
     STORE_REJECTED("가게 취소"),
-    REFUNDED("환불 완료"),
     COMPLETED("배달 완료");
 
     private final String description;
